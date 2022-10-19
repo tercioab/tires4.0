a516d493bd403bd784c88dc20e72ee2d6ade7704
walthercio Almeida <suaagenciainicial@gmail.com>
2022-10-19 01:44:19 -0300

    feat: function to return count

--- tires4.0/src/pages/teste.js
+++ tires4.0/src/pages/teste.js
@@ -1,60 +1,84 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 export default function Teste() {
-
-
-    const [form, setform] = useState([
+	const [form, setform] = useState([
         {
-            marca: '',
-            km: '',
-            price: '',
-            key: 1,
-        }
-    ])
+			marca: "",
+			km: "",
+			price: "",
+			key: Date.now(),
+		},
+    ]);
     
-    const add = () => {
+const [teste, setTeste] = useState([])
 
-        setform((prev) => [
-            ...prev,
-            { 
-                marca: '',
-                km: '',
-                price: '',
+    const add = () => {
+        
+		setform(prev => [
+			...prev,
+            {
+                
+				marca: "",
+				km: "",
+                price: "",
+				key: Date.now(),
+			},
+		]);
+	};
+
+    const on = (key, { target }) => {
+        const {name, value } = target
+		setform(prev => {
+			const newState = prev.map(product => {
+				if (product.key === key) {
+					return {
+						...product,
+						[name]: value,
+					};
+				}
+				return product;
+            });
+            return newState;
+		});
+    };
+
+    const onClick = () => {
+        setTeste(form)
+        setform([
+            {
+                marca: "",
+                km: "",
+                price: "",
                 key: Date.now(),
-            }
+            },
         ])
     }
-
-
-    const on = (key,  event ) => {
-        setform((prev) => {
-            const newState = prev.map((product) => {
-                if (product.key === key) {
-                    return {
-                        ...product,
-                        [event.target.name]: event.target.value,
-                    };
-                }
-                return product;
-            })
-            return newState;
-        }) 
-    }
-
-
-
-    return (
-        <>
-            {form.map(({ key, price, marca, km  }) =>
-                <form key={key}>
-                    <input  type="text" name="marca" value={marca} onChange={(event) => on(key, event)} />
-                    <input  type="text" name="price" value={price} onChange={(event) => on(key, event)} />
-                    <input  type="text" name="km" value={km}       onChange={(event) => on(key, event)} />
-                </form>
-
-            )}
-            <button onClick={add}> add </button>
-        </>
-        
-    )
+ 
+    
+	return (
+		<>
+			{form.map(({ key, price, marca, km }) => (
+				<form key={key}>
+					<input
+						type='text'
+						name='marca'
+						value={marca}
+						onChange={event => on(key, event)}
+					/>
+					<input
+						type='number'
+						name='price'
+						value={price}
+						onChange={event => on(key, event)}
+					/>
+					<input type='number' name='km' value={km} onChange={event => on(key, event)} />
+				</form>
+            ))}
+            {teste.map(({ price, km }) => <div>
+                <p>{price * km }</p>
+            </div>)}
+            <button onClick={onClick}>enviar</button>
+			<button onClick={add}> add </button>
+		</>
+	);
 }
