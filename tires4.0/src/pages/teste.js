99bf8d3acdb35130cb4a9454017744bf273586f7
walthercio Almeida <suaagenciainicial@gmail.com>
2022-10-19 03:00:45 -0300

    feat: function to order values

--- tires4.0/src/pages/teste.js
+++ tires4.0/src/pages/teste.js
@@ -2,32 +2,30 @@ import React, { useState } from "react";
 
 export default function Teste() {
 	const [form, setform] = useState([
-        {
+		{
 			marca: "",
 			km: "",
 			price: "",
 			key: Date.now(),
 		},
-    ]);
-    
-const [teste, setTeste] = useState([])
+	]);
 
-    const add = () => {
-        
+	const [result, setResult] = useState([]);
+
+	const addForm = () => {
 		setform(prev => [
 			...prev,
-            {
-                
+			{
 				marca: "",
 				km: "",
-                price: "",
+				price: "",
 				key: Date.now(),
 			},
 		]);
 	};
 
-    const on = (key, { target }) => {
-        const {name, value } = target
+	const onHandleChange = (key, { target }) => {
+		const { name, value } = target;
 		setform(prev => {
 			const newState = prev.map(product => {
 				if (product.key === key) {
@@ -37,48 +35,77 @@ const [teste, setTeste] = useState([])
 					};
 				}
 				return product;
-            });
-            return newState;
+			});
+			return newState;
 		});
-    };
+	};
 
-    const onClick = () => {
-        setTeste(form)
-        setform([
-            {
-                marca: "",
-                km: "",
-                price: "",
-                key: Date.now(),
-            },
-        ])
-    }
- 
-    
+	const onClick = () => {
+		const retorno = form.map(({ marca, price, km }) => {
+			const count = [];
+			count.push((( price / km ) * 1000 ).toFixed(2));
+			const finalResult = {
+				result: count,
+				marca,
+				km,
+				price,
+			};
+			return finalResult;
+		});
+		setResult(retorno);
+
+		setform([
+			{
+				marca: "",
+				km: "",
+				price: "",
+				key: Date.now(),
+			},
+		]);
+	};
+
 	return (
 		<>
 			{form.map(({ key, price, marca, km }) => (
 				<form key={key}>
-					<input
+                    <input
+                        placeholder="digite a marca"
 						type='text'
 						name='marca'
 						value={marca}
-						onChange={event => on(key, event)}
+						onChange={event => onHandleChange(key, event)}
 					/>
-					<input
+                    <input
+                        placeholder="digite o preço"
 						type='number'
 						name='price'
 						value={price}
-						onChange={event => on(key, event)}
+						onChange={event => onHandleChange(key, event)}
 					/>
-					<input type='number' name='km' value={km} onChange={event => on(key, event)} />
+                    <input
+                        placeholder="digite a kilometragem"
+						type='number'
+						name='km'
+						value={km}
+						onChange={event => onHandleChange(key, event)}
+					/>
 				</form>
-            ))}
-            {teste.map(({ price, km }) => <div>
-                <p>{price * km }</p>
-            </div>)}
-            <button onClick={onClick}>enviar</button>
-			<button onClick={add}> add </button>
+			))}
+			<button onClick={onClick}>enviar</button>
+			<button onClick={addForm}>+</button>
+            {result
+                .sort((a,b) => a.price - b.price)
+                .map(
+				({ result, marca, km, price }, index) =>
+					(price, marca, km) && (
+						<div key={index}>
+							<h3>{marca}</h3>
+							<h3>{km}</h3>
+							<h3>{price}</h3>
+							<h3>{result}</h3>
+						</div>
+					),
+			)}
 		</>
 	);
 }
